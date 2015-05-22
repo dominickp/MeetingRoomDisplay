1857a975b3b6ecab37840176b75709bb0d15376a
Dominick Peluso <peluso.dominick@gmail.com>
2015-05-22 13:26:32 -0400

    Calculations of in progress event and time until next

--- src/App/Controller/CalendarController.php
+++ src/App/Controller/CalendarController.php
@@ -40,53 +40,75 @@ class CalendarController
 
         $calendar_url = 'shawmutdelivers.com_35373038323139323533@resource.calendar.google.com';
 
-        $real = new \DateTime('tomorrow midnight');
-        $midnight =  $real->format(\DateTime::ATOM);
+        $midnight_datetime = new \DateTime('tomorrow midnight');
+        $midnight =  $midnight_datetime->format(\DateTime::ATOM);
 
         $now = date(\DateTime::ATOM);
 
-        //print_r($now.$midnight);die;
-
+        // Get events
         $service = new Google_Service_Calendar($client);
         $optParams = array(
             'timeMax' => $midnight,
             'timeMin' => $now,
             "singleEvents"=>true,
+            'orderBy' => 'startTime'
         );
 
         $events = $service->events->listEvents($calendar_url, $optParams);
-//
-//        $captured_events = array();
-//
-//        while(true) {
-//            foreach ($events->getItems() as $event) {
-//                //echo $event->getSummary();
-//                //print_r(json_encode($event));
-//                $captured_events[] = $event;
-//            }
-//            $pageToken = $events->getNextPageToken();
-//            if ($pageToken) {
-//                $optParams = array('pageToken' => $pageToken);
-//                $events = $service->calendarList->listCalendarList($optParams);
-//            } else {
-//                break;
-//            }
-//        }
+
+//print_r($events[1]); die;
+
+
+        // Find the time start of an event
+
+        function getTimeUntilNextEvent($event)
+        {
+            $first_event_start = $event->getStart()->getDateTime();
+            $first_event_start_datetime = new \DateTime($first_event_start);
+
+            $interval = $first_event_start_datetime->diff(new \DateTime());
+            $time_until_next_event =  $interval->format("%h hours, %i minutes");
+
+            return array(
+                'first_event_start' => $first_event_start,
+                'time_until_next_event' => $time_until_next_event
+            );
+        }
+
+        $no_event = array(
+            'first_event_start' => false,
+            'time_until_next_event' => false
+        );
+
+        // Get first event, may be in progress
+        if($events[0]){
+            $next_event = getTimeUntilNextEvent($events[0]);
+        } else {
+            $next_event = $no_event;
+        }
 
 
 
-        //$results = $service->calendars->get($calendar_url);
-        //$results = $service->volumes->listVolumes('Henry David Thoreau', $optParams);
 
-        //print_r(json_encode($results)); die;
+        // Determine if an event is in progress or not
+        if($next_event['first_event_start'] < $now){
+            $event_in_progress = 'yes';
+            // Next event is actually the further one
+            if($events[1]) {
+                $next_event = getTimeUntilNextEvent($events[1]);
+            } else {
+                $next_event = $no_event;
+            }
+        } else {
+            $event_in_progress = 'no';
+        }
 
-        $test = new \Google_Service_Calendar_Event();
-        $start = $test->getStart();
-            //$start->
 
 
         return $this->twig->render('index.twig', array(
-            'events' => $events
+            'events' => $events,
+            'time_until_next_event' => $next_event['time_until_next_event'],
+            'event_in_progress' => $event_in_progress
         ));
     }
 }
